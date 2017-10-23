c64bd25bdfa3c869a5cb6b6015945ea9038abc01
Bruno Marques <150221053@estudantes.ips.pt>
2017-10-23 19:32:37 +0100

    adicionado comentario

--- VSHelloWorld_150221053/Program.cs
+++ VSHelloWorld_150221053/Program.cs
@@ -12,6 +12,7 @@ namespace VSHelloWorld_150221053
 {
     public class Program
     {
+        //comentario De teste
         public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
